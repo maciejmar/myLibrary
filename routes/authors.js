669c277beb1c2e10278e4a93a8e3f5bb5f6cd236
Maciej Marosz <48849533+maciejmar@users.noreply.github.com>
2021-07-04 23:38:32 +0200

    initial routes

---
+++ routes/authors.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const Author = require('../models/author');
+
+
+//new author route
+router.get('/new', (req,res) => {
+res.render('authors/new', {author: new Author() })
+});
+
+//all authors route
+router.get('/', async (req,res) => {
+    let searchConstrains = {};
+    if(req.query.name != null && req.query.name !=''){
+        searchConstrains.name = new RegExp(req.query.name, 'i');
+        console.log("searchConstrains = ", searchConstrains.name);
+    }
+    try{
+        const authors = await Author.find(searchConstrains);
+        res.render('authors/index', {
+            authors: authors,
+            searchConstrains: req.query
+        })
+    }
+    catch{
+       res.redirect('/');
+    }
+});
+
+// Create Author Route
+router.post('/', async (req, res) => {
+    const author = new Author({
+      name: req.body.name
+    })
+    try{
+        const newAuthor = await author.save();
+        //res.redirect(`authors/${newAuthor.id}`);
+        res.redirect('authors');
+    }catch{
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        });
+    }
+   /*  try {
+      const newAuthor = await author.save()
+      // res.redirect(`authors/${newAuthor.id}`)
+      res.redirect(`authors`)
+    } catch {
+      res.render('authors/new', {
+        author: author,
+        errorMessage: 'Error creating Author'
+      })
+    } */
+  })
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,15 +4,20 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+const bodyParser = require('body-Parser');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false, limit:'10mb' }));
+ 
 
 const mongoose = require('mongoose');
+
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser:true,
     useUnifiedTopology: true });
@@ -21,5 +26,6 @@ mongoose.connect(process.env.DATABASE_URL, {
     db.once('open', () => console.log('Connected to database'))
 
 app.use('/',indexRouter);
+app.use('/authors',authorRouter);
 
 app.listen(process.env.PORT || 3000);
