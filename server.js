b469d1a86ba8343b195db2c02f668c426fa784f4
Maciej Marosz <48849533+maciejmar@users.noreply.github.com>
2021-07-05 09:06:22 +0200

    few changes in syntax

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
 const bodyParser = require('body-parser');
 
-app.set('view engine', 'ejs');
+app.set('view engine' , 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
@@ -23,7 +23,7 @@ mongoose.connect(process.env.DATABASE_URL, {
     useUnifiedTopology: true });
     const db = mongoose.connection;
     db.on('error', error => console.error(error));
-    db.once('open', () => console.log('Connected to database'))
+    db.once('open', () => console.log('Connected to the database'))
 
 app.use('/',indexRouter);
 app.use('/authors',authorRouter);
